c2c0a04b70ea533a157da7010871d7d34b84c18a
filipearray <codedforgetbro@gmail.com>
2023-01-12 23:07:52 -0300

    step 5

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const tempClient = {
 }; // Step 1: Essa constante será criada para não interagir com os dados da tela. Quando nós chamarmos o createClient, nós passaremos esse tempClient.
 
 const createClient = (client) => {
-    const db_client = [];
+    const db_client = JSON.parse(localStorage.getItem("db_client"))
 
     db_client.push(client);
 
@@ -19,5 +19,9 @@ const createClient = (client) => {
 // Step 3: Há um porém, quando se trata de objetos serem usados como argumento para serem guardados, a key só aceita STRINGS. Mas isso pode ser facilmente resolvido com o JSON.stringify().
 
 // Step 4: O envio está funcionando, mas ao mudar os dados do tempClient - de forma bruta, mudando manualmente as values - o db_client ao invés de criar um novo Client na database, ele substitui as informações do anterior, tem algo errado. Para mudar isso podemos colocar esses dados em um ARRAY e então fazer um push sempre que houver novos objetos. Não esqueça que mudamos a value de client para db_client da key "db_client"
+
+// Step 5: Porém, continuou substituindo. Isso porque estamos pegando a partir de um array vazio, e isso não pode acontecer. O ideal seria ler o que já está no banco de dados. com o LS get. Ele lerá o banco e adicionará com PUSH o novo cliente.
+// Após fazer isso, perceberá que ainda há algo errado. Ele traz os dados, mas em STRING. Então transforme novamente o LS get em JSON com o parse ao invés do stringify.
+// A partir de agora, enquanto você estiver mudando os dados de forma manual e bruta, você perceberá que ao invés de substituir, agora ele vai acumular os dados.
 
 
