7622b2451ec05a07706dd78e82e2e75e92d9d715
filipearray <codedforgetbro@gmail.com>
2023-01-16 21:03:12 -0300

    step 10

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ const tempClient = {
 const getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? [];
 const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
 
+// DELETE
+const deleteClient = (index) => {
+    const dbClientDeleted = readClient();
+    dbClientDeleted.splice(index, 1);
+    setLocalStorage(dbClientDeleted);
+};
+/* Step 10: O splice funciona da seguinte maneira. O splice() precisa de pelo menos um parâmetro, que é o index de início, onde a operação de splice começa. 
+
+let meses = ["Janeiro", "Fevereiro", "Segunda", "Terça"];
+let dias = meses.splice(2);
+console.log(dias); ["Segunda", "Terça"] 
+
+No código acima, o número 2 é passado para o método, o que significa que o splice() começará a remover elementos a partir do índice 2. 
+
+Você também pode definir quantos elementos deseja remover do array, passando um segundo argumento numérico conhecido como contador de remoção. No dbClientDeleted, é 1. Então digamos que o index vai ser 2, então ele vai, i0? Não, i1? Não, i2? Sim! i3? Não. Um suposto index 3 seria exclúido se o removeCount fosse 2. Uma vez que você omite o removeCount, ele vai apagar todos os indexes depois do index declarado. */
+
+
 // UPDATE
 const updateClient = (index, client) => {
     const dbClientUpdated = readClient();
